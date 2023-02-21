804853fb584dd10335a95b2ccc76d7e303ff5ec4
Renan Moreira <renan.tsx@gmail.com>
2023-02-21 14:35:11 -0300

    Solução dos problemas

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # database-query
-Desafio: Realizará consultas no banco de dados com o TypeORM de três maneiras:
+Desafio: Realizará consultas no banco de dados com o TypeORM de três maneiras.
+
+Abrir projeto com VSCode Online:
+
+https://github1s.com/MyIgnite/CRUD-nodejs
+
+Clone o projeto, navegue até a raiz do projeto e execute:</br>
+
+`yarn` </br>
+
+Execute o comando para testar a aplicação </br>
+`yarn test` </br> </br>
+
+- [x] should be able to find user with games list by user's ID
+- [x] should be able to list users ordered by first nam
+- [x] should be able to find user by full nam
+- [x] should be able find a game by entire or partial given titl
+- [x] should be able to get the total count of game
+- [x] should be able to list users who have given game i
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository } from 'typeorm';
 
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { IFindUserByFullNameDTO, IFindUserWithGamesDTO } from '../../dtos';
 import { User } from '../../entities/User';
 import { IUsersRepository } from '../IUsersRepository';
 
@@ -15,16 +15,33 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const userWithGames = await this.repository.findOneOrFail({
+      relations: ["games"],
+      where: {
+        id: user_id
+      }
+    });
+
+    return userWithGames;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    const query = "SELECT * FROM users ORDER BY first_name ASC";
+    const users = await this.repository.query(query);
+    return users;
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    // Retornar usuário que possua first_name e last_name
+    // Ignorar caixa alta
+
+    const query = "SELECT * FROM users WHERE LOWER(first_name) = LOWER($1) AND LOWER(last_name) = LOWER($2)";
+    const users = await this.repository.query(query, [first_name, last_name])
+    return users; 
   }
 }
